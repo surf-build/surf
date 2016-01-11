083552fcd365660430a3bf74177b948cff983f0c
Paul Betts <paul@paulbetts.org>
2016-01-11 10:30:58 -0800

    Add an optional way to limit concurrency

--- src/promise-array.js
+++ src/promise-array.js
@@ -1,12 +1,12 @@
 import {Observable} from 'rx';
 
-export function asyncMap(array, selector) {
+export function asyncMap(array, selector, maxConcurrency=4) {
   return Observable.from(array)
-    .map((k) => 
-      Observable.defer(() => 
+    .map((k) =>
+      Observable.defer(() =>
         Observable.fromPromise(selector(k))
           .map((v) => ({ k, v }))))
-    .merge(4)
+    .merge(maxConcurrency)
     .reduce((acc, kvp) => {
       acc[kvp.k] = kvp.v;
       return acc;
