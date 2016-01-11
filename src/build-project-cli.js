ea50100489e6607ae7231a401eb99d464ee8bd4a
Paul Betts <paul@paulbetts.org>
2016-01-11 00:15:42 -0800

    Comments Are Cool

--- src/build-project-cli.js
+++ src/build-project-cli.js
@@ -1,11 +1,21 @@
 import './babel-maybefill';
 
 import _ from 'lodash';
+import crypto from 'crypto';
+import path from 'path';
+import pify from 'pify';
+import {Repository, FetchOptions, Clone, CloneOptions} from 'nodegit';
+
+const [rimraf, mkdirp] = _.map(['rimraf', 'mkdirp'], (x) => pify(require(x)));
 
 const d = require('debug')('serf:serf-build');
 const argv = require('yargs').argv;
 
 async function main() {
+  // Checkout a bare repo to $SECRET_DIR if necessary
+  // Clone a new copy to a work dir
+  // Find a builder, run that shit
+  // Copy artifacts to $ARTIFACTS_DIR
 }
 
 main()
