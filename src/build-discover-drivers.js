ff8d8a1cb1bc4bbc1383b35e97937ceb3dccce4f
Paul Betts <paul@paulbetts.org>
2016-01-11 17:14:25 -0800

    :: paranoia intensifies ::

--- src/build-discover-drivers.js
+++ src/build-discover-drivers.js
@@ -2,11 +2,12 @@ import path from 'path';
 import {fs} from './promisify';
 import BuildDiscoverBase from './build-discover-base';
 
+const d = require('debug')('serf:build-discover-drivers');
+
 const possibleScriptPathsWin32 = [
   'script/ci.ps1',
   'build.cmd'
 ];
-
 
 const possibleScriptPathsPosix = [
   'script/ci',
@@ -24,12 +25,17 @@ export class BuildScriptDiscoverer extends BuildDiscoverBase {
     for (let guess of guesses) {
       try {
         let fullPath = path.join(this.rootDir, guess);
-        return await fs.stat(fullPath);
+        d(`Looking for file ${fullPath}`);
+        let stat = await fs.stat(fullPath);
+
+        d("Found it!");
+        if (stat) return fullPath;
       } catch (e) {
         continue;
       }
     }
 
+    d("Didn't find a build script");
     return null;
   }
 
