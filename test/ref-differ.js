c58c6548ef78c83c154bec157a0ab14d5418e914
Paul Betts <paul@paulbetts.org>
2016-01-18 14:22:37 -0800

    More tests

--- test/ref-differ.js
+++ test/ref-differ.js
@@ -5,6 +5,15 @@ import path from 'path';
 import {fs} from '../src/promisify';
 import determineChangedRefs from '../src/ref-differ';
 
+const d = require('debug')('serf-test:ref-differ');
+
+function getSeenRefs(refs) {
+  return _.reduce(refs, (acc, x) => {
+    acc.add(x.object.sha);
+    return acc;
+  }, new Set());
+}
+
 describe.only('ref differ', function() {
   beforeEach(async function() {
     let acc = {};
@@ -21,17 +30,39 @@ describe.only('ref differ', function() {
   });
 
   it('should find one changed ref between refs1 and refs2', function() {
-    let seen = _.reduce(this.refExamples['refs1.json'], (acc, x) => {
-      acc.add(x.object.sha);
-      return acc;
-    }, new Set());
+    let seen = getSeenRefs(this.refExamples['refs1.json']);
 
     let result = determineChangedRefs(
       seen,
       this.refExamples['refs1.json'],
       this.refExamples['refs2.json']);
 
+    d(JSON.stringify(_.map(result, (x) => x.ref)));
     expect(result.length).to.equal(1);
     expect(result[0].object.sha).to.equal('a5971a6c6943154d241ad002b7240e96fa0375e6');
+  });
+
+  it('should find two changed ref between refs1 and refs3', function() {
+    let seen = getSeenRefs(this.refExamples['refs1.json']);
+
+    let result = determineChangedRefs(
+      seen,
+      this.refExamples['refs1.json'],
+      this.refExamples['refs3.json']);
+
+    d(JSON.stringify(_.map(result, (x) => x.ref)));
+    expect(result.length).to.equal(3);
+  });
+
+  it('should find one changed ref between refs3 and refs4', function() {
+    let seen = getSeenRefs(this.refExamples['refs1.json']);
+
+    let result = determineChangedRefs(
+      seen,
+      this.refExamples['refs3.json'],
+      this.refExamples['refs4.json']);
+
+    d(JSON.stringify(_.map(result, (x) => x.ref)));
+    expect(result.length).to.equal(1);
   });
 });
