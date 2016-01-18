f89836956ca68c75ab5a77d3bba070fb4e7b7b55
Paul Betts <paul@paulbetts.org>
2016-01-18 11:39:28 -0800

    notes

--- src/ref-differ.js
+++ src/ref-differ.js
@@ -1,6 +1,10 @@
 import _ from 'lodash';
 
-export default function determineChangedRefs(seenCommits, refInfo) {
+export default function determineChangedRefs(seenCommits, previousRefInfo, refInfo) {
+  // List of ref => SHA1
+  // Refs added / refs deleted / refs changed
+  // Result should be list of SHA1s to build, and list of SHA1s to cancel
+
   return _.filter(refInfo, (ref) => {
     if (seenCommits.has(ref.object.sha)) return false;
     seenCommits.add(ref.object.sha);
