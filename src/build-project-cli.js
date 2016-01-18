a5f286447b9d38e598135fcd9d2c2a832bed4a4e
Paul Betts <paul@paulbetts.org>
2016-01-18 14:43:55 -0800

    spawn now returns an Observable

--- src/build-project-cli.js
+++ src/build-project-cli.js
@@ -93,7 +93,7 @@ async function main() {
   let buildOutput = null;
 
   try {
-    buildOutput = await runBuildCommand(cmd, args, workDir, sha);
+    buildOutput = await runBuildCommand(cmd, args, workDir, sha).toPromise();
     console.log(buildOutput);
     buildPassed = true;
   } catch (e) {
