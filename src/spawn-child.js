d2a240030863f9c2666a645a22b69e1dd6dd81ec
Paul Betts <paul@paulbetts.org>
2016-01-20 22:43:55 -0800

    Don't truncate build output

--- src/spawn-child.js
+++ src/spawn-child.js
@@ -6,7 +6,9 @@ var argv = process.argv.splice(3);
 var proc = cp.spawn(cmd, argv, { detached: false, stdio: ['pipe', 'inherit', 'inherit'], env: process.env, cwd: undefined });
 
 proc.on('exit', function(code) {
-  process.exit(code);
+  // NB: stdout will actually keep writing after exit fires,
+  // so we need to stall for a bit or else we'll lose info
+  setTimeout(() => process.exit(code), 5*1000);
 });
 
 proc.on('error', function(e) {
