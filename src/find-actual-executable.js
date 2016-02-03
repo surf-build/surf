77d543975682c73638776ebff80c7cfd743d58f9
Paul Betts <paul@paulbetts.org>
2016-02-02 23:15:57 -0800

    Fix finding executables on Windows

--- src/find-actual-executable.js
+++ src/find-actual-executable.js
@@ -1,8 +1,22 @@
+import _ from 'lodash';
 import path from 'path';
+import sfs from 'fs';
 
 export default function findActualExecutable(fullPath, args) {
   // POSIX can just execute scripts directly, no need for silly goosery
   if (process.platform !== 'win32') return { cmd: fullPath, args: args };
+  
+  // NB: When you write something like `surf-client ... -- surf-build` on Windows,
+  // a shell would normally convert that to surf-build.cmd, but since it's passed
+  // in as an argument, it doesn't happen
+  if (!sfs.existsSync(fullPath)) {
+    const possibleExts = ['.exe', '.bat', '.cmd', '.ps1'];
+    let realExecutable = _.find(possibleExts, (x) => sfs.existsSync(`${fullPath}${x}`));
+    
+    if (realExecutable) {
+      return findActualExecutable(realExecutable, args);
+    }
+  }
 
   if (fullPath.match(/\.ps1$/i)) {
     let cmd = path.join(process.env.SYSTEMROOT, 'System32', 'WindowsPowerShell', 'v1.0', 'PowerShell.exe');
