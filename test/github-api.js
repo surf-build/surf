8514b227d855229fefdb547d0a31727231be6bba
Paul Betts <paul@paulbetts.org>
2016-02-12 15:06:42 -0800

    Add some tests

---
+++ test/github-api.js
@@ -0,0 +1,21 @@
+import './support';
+
+import _ from 'lodash';
+import path from 'path';
+import { determineInterestingRefs } from '../src/github-api';
+import { fs } from '../src/promisify';
+
+const d = require('debug')('surf-test:github-api');
+
+describe.only('the determineInterestingRefs method', function() {
+  beforeEach(async function() {
+    this.refInfo = JSON.parse(await fs.readFile(path.join(__dirname, 'commit_info.example.json')));
+  });
+
+  it('shouldnt have pull #13 in it because its already a branch', function() {
+    let result = determineInterestingRefs(this.refInfo);
+
+    //d(JSON.stringify(result));
+    expect(_.find(result, (x) => x.ref === 'refs/pull/13/head')).not.to.be.ok;
+  });
+});
