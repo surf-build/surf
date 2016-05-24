27178f066d58fb89c9d59641e1473228041ca25f
Paul Betts <paul@paulbetts.org>
2016-05-24 12:07:55 -0700

    /shrug

--- src/git-api.js
+++ src/git-api.js
@@ -191,7 +191,7 @@ export async function addFilesToGist(repoUrl, targetDir, artifactDir, token=null
   let repo = await Repository.open(targetDir);
 
   d("Opening index");
-  let idx = repo.index;
+  let idx = await repo.index();
   await idx.read(1);
 
   d("Reading artifacts directory");
