5f131ef72310ad230015ca340535be6f44228d5d
Paul Betts <paul@paulbetts.org>
2016-07-21 15:17:18 -0700

    Don't run Danger if Ruby or Bundler isn't in PATH

--- src/build-discoverers/danger.js
+++ src/build-discoverers/danger.js
@@ -1,6 +1,7 @@
 import path from 'path';
 import {statNoException} from '../promise-array';
 import BuildDiscoverBase from '../build-discover-base';
+import findActualExecutable from '../find-actual-executable';
 
 const d = require('debug')('surf:build-discover-npm');
 
@@ -17,6 +18,11 @@ export default class DangerBuildDiscoverer extends BuildDiscoverBase {
     let exists = await statNoException(dangerFile);
 
     if (process.env.SURF_DISABLE_DANGER) return 0;
+    
+    // If we can't find Ruby or Bundler in PATH, bail
+    if (!['ruby', 'bundler'].find((x) => findActualExecutable(x, []).cmd === x)) {
+      return 0;
+    }
     
     if (exists) { d(`Found Dangerfile at ${dangerFile}`); }
     return exists ? 100 : 0;
