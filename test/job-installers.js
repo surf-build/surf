065b929f14520b95640903959e6bd00ea266ae1a
Paul Betts <paul@paulbetts.org>
2016-09-01 15:49:54 -0700

    Add tests for Docker

--- test/job-installers.js
+++ test/job-installers.js
@@ -1,5 +1,7 @@
 import './support';
+
 import SystemdInstaller from '../src/job-installers/systemd';
+import DockerInstaller from '../src/job-installers/docker';
 import {installJob} from '../src/job-installer-api';
 
 const d = require('debug')('surf-test:job-installers');
@@ -11,11 +13,7 @@ describe('systemd job installer', function() {
     this.sampleCommand = 'surf-build -r https://github.com/surf-build/example-csharp -- surf-build -n "surf"';
   });
 
-  if (process.platform !== 'linux') return;
-
   it('should capture Surf environment variables', async function() {
-    expect((await this.fixture.getAffinityForJob(this.sampleName, this.sampleCommand)) > 0).to.be.ok;
-
     process.env.SURF_TEST_ENV_VAR = 'hello';
     let result = await this.fixture.installJob(this.sampleName, this.sampleCommand, true);
     delete process.env.SURF_TEST_ENV_VAR;
@@ -29,8 +27,6 @@ describe('systemd job installer', function() {
   });
 
   it('should have the command as ExecPath', async function() {
-    expect((await this.fixture.getAffinityForJob(this.sampleName, this.sampleCommand)) > 0).to.be.ok;
-
     let result = await this.fixture.installJob(this.sampleName, this.sampleCommand, true);
 
     let execStartLine = result.split('\n').find((l) => l.match(/ExecStart/));
@@ -38,12 +34,40 @@ describe('systemd job installer', function() {
   });
 
   it('should ensure that absolute paths are rooted', async function() {
-    expect((await this.fixture.getAffinityForJob(this.sampleName, this.sampleCommand)) > 0).to.be.ok;
-
     let result = await this.fixture.installJob(this.sampleName, 'ls -al', true);
 
     let execStartLine = result.split('\n').find((l) => l.match(/ExecStart/));
     expect(execStartLine.indexOf('/usr/bin/ls') > 0).to.be.ok;
+  });
+});
+
+describe('docker job installer', function() {
+  beforeEach(function() {
+    this.fixture = new DockerInstaller();
+    this.sampleName = 'example-csharp';
+    this.sampleCommand = 'surf-build -r https://github.com/surf-build/example-csharp -- surf-build -n "surf"';
+  });
+  
+  it('should capture Surf environment variables', async function() {
+    process.env.SURF_TEST_ENV_VAR = 'hello';
+    let result = await this.fixture.installJob(this.sampleName, this.sampleCommand, true);
+    delete process.env.SURF_TEST_ENV_VAR;
+
+    d(result);
+
+    expect(
+      result.split('\n')
+        .find((l) => l.match(/ENV .*SURF_TEST_ENV_VAR.*hello/))
+    ).to.be.ok;
+  });
+
+  it('should have the command as CMD', async function() {
+    expect((await this.fixture.getAffinityForJob(this.sampleName, this.sampleCommand)) > 0).to.be.ok;
+
+    let result = await this.fixture.installJob(this.sampleName, this.sampleCommand, true);
+
+    let execStartLine = result.split('\n').find((l) => l.match(/CMD/));
+    expect(execStartLine.indexOf(this.sampleCommand.split(' ')[0]) > 0).to.be.ok;
   });
 });
 
