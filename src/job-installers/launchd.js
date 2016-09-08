98a37e8c154b14eeaaea4243f807a32c5c38831d
Paul Betts <paul@paulbetts.org>
2016-09-08 12:23:44 -0700

    First hack at a launchd agent

---
+++ src/job-installers/launchd.js
@@ -0,0 +1,63 @@
+import fs from 'fs';
+import _ from 'lodash';
+
+import JobInstallerBase from '../job-installer-base';
+import {statNoException} from '../promise-array';
+import {findActualExecutable, spawnPromise} from 'spawn-rx';
+import stringArgv from 'string-argv';
+import xmlescape from 'xml-escape';
+
+const d = require('debug')('surf:launchd');
+
+// NB: This has to be ../src or else we'll try to get it in ./lib and it'll fail
+const makeLaunchdService = 
+  _.template(fs.readFileSync(require.resolve('../../src/job-installers/launchd.plist.in'), 'utf8'));
+
+export default class LaunchdInstaller extends JobInstallerBase {
+  getName() {
+    return 'launchd';
+  }
+  
+  async getAffinityForJob(name, command) {
+    return process.platform === 'darwin' ? 5 : 0;
+  }
+
+  async installJob(name, command, returnContent=false) {
+    // NB: launchd requires commands to be have absolute paths
+    let [, cmd, params] = command.match(/^(\S+)(.*)/);
+    command = findActualExecutable(cmd, []).cmd + params;
+    
+    let opts = {
+      commandWithoutArgs: command,
+      argList: stringArgv(params),
+      envs: this.getInterestingEnvVars().map((x) => [x, process.env[x]]),
+      name
+    };
+        
+    opts = Object.keys(opts).reduce((acc, x) => {
+      acc[x] = xmlescape(opts[x]);
+      return acc;
+    }, {});
+    
+    if (returnContent) {
+      let ret = {};
+      ret[`local.${name}.plist`] = makeLaunchdService(opts);
+      return ret;
+    } 
+    
+    let target = `${process.env.HOME}/Library/LaunchAgents/local.${name}.plist`;
+    
+    fs.writeFileSync(target, makeLaunchdService(opts));
+    fs.chmodSync(target, 0644);
+    
+    await spawnPromise('systemctl', ['daemon-reload']);
+    await spawnPromise('systemctl', ['start', name]);
+    
+    return `launchd agent written to '${target}
+  
+launchd agents only run when the current user logs on, because many macOS services
+only work interactively, such as the keychain.
+
+To start manually, run launchctl load ${target}`;
+  }
+}
