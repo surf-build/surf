bc35e99f0b71a0a1d79e65b236b22ac33ee30bbd
Paul Betts <paul@paulbetts.org>
2016-10-13 14:34:21 -0700

    Add implicit repo param

--- src/clean-workdirs-main.js
+++ src/clean-workdirs-main.js
@@ -1,8 +1,8 @@
 import _ from 'lodash';
 import { asyncMap } from './promise-array';
 import { rimraf } from './promisify';
-import { getAllWorkdirs } from './git-api';
-import { fetchAllRefsWithInfo, getNwoFromRepoUrl } from './github-api';
+import { getAllWorkdirs, getOriginForRepo } from './git-api';
+import { fetchAllRefsWithInfo, getNwoFromRepoUrl, getSanitizedRepoUrl } from './github-api';
 
 const d = require('debug')('surf:surf-clean');
 
@@ -11,12 +11,17 @@ export default async function main(argv, showHelp) {
     showHelp();
     process.exit(0);
   }
-  
-  if (!argv.s || !argv.r) {
+
+  let repo = argv.repo || process.env.SURF_REPO;
+  if (!repo) {
+    repo = getSanitizedRepoUrl(await getOriginForRepo('.'));
+  }
+
+  if (!repo) {
     showHelp();
     process.exit(-1);
   }
-  
+
   // Do an initial fetch to get our initial state
   let refInfo = null;
 
@@ -29,13 +34,13 @@ export default async function main(argv, showHelp) {
   }
 
   let safeShas = _.map(refInfo, (ref) => `-${ref.object.sha.substr(0,6)}-`);
-  
+
   d(`safeShas: ${Array.from(safeShas).join()}`);
   let allDirs = await getAllWorkdirs(argv.r);
   let toDelete = _.filter(
-    allDirs, 
+    allDirs,
     (x) => !_.find(safeShas, (sha) => x.indexOf(sha) > 0));
-  
+
   if (argv['dry-run']) {
     _.each(toDelete, (x) => console.log(x));
   } else {
