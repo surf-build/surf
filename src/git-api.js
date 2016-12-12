56919d5ba533e45284befc00ac835323236dce95
Paul Betts <paul@paulbetts.org>
2016-12-12 13:35:41 -0800

    Switch based on the environment

--- src/git-api.js
+++ src/git-api.js
@@ -1,1 +1,13 @@
-module.exports = require('./git-nodegit-api');
+import {findActualExecutable} from 'spawn-rx';
+
+const d = require('debug')('surf:git-api');
+
+const shouldUseGitExe = process.env.SURF_FORCE_GIT_EXECUTABLE || findActualExecutable('git', []).cmd !== 'git';
+
+if (shouldUseGitExe && !process.env.SURF_FORCE_NODE_GIT) {
+  d('Using git executable');
+  module.exports = require('./git-cmd-api');
+} else {
+  d('Using libgit2');
+  module.exports = require('./git-nodegit-api');
+}
