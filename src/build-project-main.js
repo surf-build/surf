9e3ac785409990a95110819d066ccfb7fd0a5c27
Paul Betts <paul@paulbetts.org>
2017-01-05 14:55:38 -0800

    node-git locks our Gist folder

--- src/build-project-main.js
+++ src/build-project-main.js
@@ -225,9 +225,8 @@ async function realMain(argv, showHelp) {
 
     try {
       d(`Uploading build artifacts using token: ${token}`);
-      let targetDir = await retryPromise(() => 
+      await retryPromise(() => 
         uploadBuildArtifacts(gistInfo.result.id, gistInfo.result.git_pull_url, artifactDirs, buildLog, token));
-      await rimraf(targetDir);
     } catch (e) {
       console.error(`Failed to upload build artifacts: ${e.message}`);
       d(e.stack);
